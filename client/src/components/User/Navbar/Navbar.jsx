9567749d6d3ca8ca383360c507ab0daf6d1fc485
Velin Apostolov <velin.apostolove@gmail.com>
2024-08-18 16:29:23 +0300

    added searchbar and logo

--- client/src/components/User/Navbar/Navbar.jsx
+++ client/src/components/User/Navbar/Navbar.jsx
@@ -1,37 +1,61 @@
-import { Menu } from "antd";
+import { useState, useEffect } from "react";
+import { Menu, Input } from "antd";
 import { useNavigate } from "react-router-dom";
+
+const { Search } = Input;
 
 const menuItems = [
     { key: '1', label: 'Products', },
-    { key: '2', label: 'Profile', },
-    { key: '3', label: 'Wishlist', },
-    { key: '4', label: 'Cart', },
-    { key: '5', label: 'Register', },
-    { key: '6', label: 'Login', },
-    { key: '7', label: 'Profile', },
+    { key: '2', label: 'Wishlist', },
+    { key: '3', label: 'Cart', },
+    { key: '4', label: 'Register', },
+    { key: '5', label: 'Login', },
+    { key: '6', label: 'Profile', },
 ];
 
 const routes = {
-    1: '/',
-    2: '/products',
-    3: '/wishlist',
-    4: '/cart',
-    5: '/register',
-    6: '/login',
-    7: '/profile',
+    1: '/products',
+    2: '/wishlist',
+    3: '/cart',
+    4: '/register',
+    5: '/login',
+    6: '/profile',
 };
 
 const Navbar = () => {
+    const initialKey = Object.keys(routes).find(key => routes[key] === location.pathname);
+    const [current, setCurrent] = useState(initialKey || null);
     const navigate = useNavigate();
 
+    useEffect(() => {
+        const key = Object.keys(routes).find(key => routes[key] === location.pathname) || null;
+        setCurrent(key);
+    }, [location.pathname]);
+
     return (
-        <div className='nav-container p-4 bg-dark-blue'>
+        <div className='nav-container p-4 bg-dark-blue flex items-center justify-between'>
+            <img
+                src='logo-no-background.svg'
+                alt="Logo"
+                className="cursor-pointer h-8 mr-4"
+                onClick={() => navigate('/')}
+            />
+            <Search
+                placeholder="Search..."
+                onSearch={(value) => console.log(value)}
+                style={{ width: 300 }}
+                className="mx-4"
+            />
             <Menu
                 theme="dark"
                 mode="horizontal"
                 items={menuItems}
+                selectedKeys={current ? [current] : []}
                 className="flex justify-end gap-4"
-                onClick={((item) => navigate(routes[item.key]))}>
+                onClick={((item) => {
+                    setCurrent(item.key);
+                    navigate(routes[item.key]);
+                })}>
             </Menu>
         </div>
     )
