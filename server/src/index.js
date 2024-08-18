962fc4b459c553ce0b9b8a5e776ab4f6cfb0ef29
Velin Apostolov <velin.apostolove@gmail.com>
2024-08-18 10:45:30 +0300

    added urlencoded middleware

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const mongoose = require('mongoose');
 require('dotenv').config();
 const dbConnect = require('./config/database');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(express.urlencoded({ extended: false }));
 
 dbConnect();
 
